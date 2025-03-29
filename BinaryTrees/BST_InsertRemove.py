946e0421d32b972146290fc3554b79eca4958dfb
Siddhant Sapru <siddhantsapru@Siddhants-MacBook-Pro.local>
2025-03-29 16:51:17 -0400

    Updated remove node file

--- BinaryTrees/BST_InsertRemove.py
+++ BinaryTrees/BST_InsertRemove.py
@@ -1,12 +1,14 @@
-# Binary Search Tree - Insert and Remove
+"""
+Binary Search Tree - Insert and Remove
 
-# IMPORTANT Algorithm - watch video to refresh memory
-    # https://neetcode.io/courses/dsa-for-beginners/18
+IMPORTANT Algorithm - watch video to refresh memory
+    https://neetcode.io/courses/dsa-for-beginners/18
 
-# Why not have the a Sorted array vs Binary search tree?
-    # Traversing to find an element is still O(n) in both
-    # But the real difference is in Insertion and Removal
-    # BST have O(logn) and array have O(nlogn)
+Why not have the a Sorted array vs Binary search tree?
+    Traversing to find an element is still O(n) in both
+    But the real difference is in Insertion and Removal
+    BST have O(logn) and array have O(nlogn)
+"""
 
 def insert(root, val):    
     if root is None:
@@ -18,8 +20,8 @@ def insert(root, val):
         root.left = insert(root.left, val)
     return root
 
-
 def remove(root, val):
+
     if root is None:
         return None
     
@@ -28,22 +30,29 @@ def remove(root, val):
     elif val < root.val:
         root.left = remove(root.left, val)
     # Up till here, we are just trying to locate the node which is equal to "val"
-    # Thats why we recursively call the remove() to find that node
-    # Now, below we are going to try to remove that particular node
+
     else:
-        # We are in here because the node value == val
-        if not root.left:
-            return root.right
-        elif not root.right:
-            return root.left
+        # Found the val to be removed
+        if not root.right:
+            return root.left        # For 1 child node only
+        elif not root.left:
+            return root.right       # For 1 child node only
         else:
+            # For 2 child nodes
+
+            ## IMPORTANT ##
+            # We only need minNode's Value to replace it with the root.val
             minNode = minValueNodeIterative(root.right)
             root.val = minNode.val
+
+            # Make sure to have it assigned to root.right
+            # Or else you will lose the updated tree structure
+            # FOR DEMO: Draw a 5 depth tree, and make sure the minNode 
+            # has a right node ATLEAST to go through this scenario
             root.right = remove(root.right, minNode.val)
 
     return root
-
-
+        
 
 def minValueNodeIterative(root):
     curr = root
@@ -52,25 +61,20 @@ def minValueNodeIterative(root):
     
     return curr
     
-    # This way is correct too
-    # But this doesn't preserve "root" in case you want to return root
+    # The below way is correct too
+    # But it doesn't preserve "root" in case you want to return root
     # Use the above way, curr = root
     while root.left:
         root = root.left
     return root
 
 def minValueNodeRecursive(root):
-    # In recursive, we don't need to do curr = root
-    # Because in recursion, you're preserving "root"
-    # as you're just going down the stack call
+    """
+    In recursive, we don't need to do curr = root
+    Because in recursion, you're preserving "root"
+    as you're just going down the stack call 
+    """
     if root.left is None:
         return root
     
     return minValueNodeRecursive(root.left)
-
-def minValue(root):
-    curr = root
-    while curr and curr.left:
-        curr = curr.left
-    
-    return curr
