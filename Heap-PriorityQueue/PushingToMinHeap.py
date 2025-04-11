98916b9b9e28b0229ed6062024bb5f71c622bad4
Siddhant Sapru <sapru.siddhant98@gmail.com>
2025-04-11 11:54:11 -0400

    Adding Pushing to Min Heap code

--- Heap-PriorityQueue/PushingToMinHeap.py
+++ Heap-PriorityQueue/PushingToMinHeap.py
@@ -1,6 +1,11 @@
 # https://neetcode.io/courses/dsa-for-beginners/24
 # Time complexity = O(logN)
 
+# For Heaps, always make sure to satisfy below 2 properties:
+    # Structure Property -> All nodes should be filled from left to right
+    # Order Property -> Follow it as per Min Heap or Max Heap
+
+
 class Heap:
     def __init__(self):
         # First element of a heap has to be a Zero (in the array representation)
